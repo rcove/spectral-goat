fa4beeda2e3f36ee5b37518c2605ec4ad5467871
Richard Cove <39362230+rcove@users.noreply.github.com>
2022-11-30 12:41:05 +1100

    Update subnets.tf

--- src/subnets.tf
+++ src/subnets.tf
@@ -4,9 +4,3 @@ resource "azurerm_subnet" "terraform_external_subnet" {
   virtual_network_name = azurerm_virtual_network.terraform_vnet.name
   address_prefixes     = [var.external_subnet_address_prefixes]
 }
-resource "azurerm_subnet" "terraform_internal_subnet" {
-  name                 = var.internal_subnet_name
-  resource_group_name  = azurerm_resource_group.terraform_rg.name
-  virtual_network_name = azurerm_virtual_network.terraform_vnet.name
-  address_prefixes     = [var.internal_subnet_address_prefixes]
-}
